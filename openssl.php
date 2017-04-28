68479d2c252714b59a1aa1f5e6a939dc8a0a1beb
Harris Marfel <hrace009@gmail.com>
2017-04-28 10:39:29 +0700

    Update openssl.php
    Add class with saving chipper

--- openssl.php
+++ openssl.php
@@ -1,22 +1,47 @@
 <?php
-$data = 'This Text Will Encrypt and Decrypt using OpenSSL!!'; // Define text will encrypted.
-$secretKey = 'somerandomkey'; // Define some random key.
-$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC')); // Define your initialization vector.
+/**
+* Main Code By: Khucing Ithem : https://www.facebook.com/backcat
+* Make with Class by: hrace009
+**/
+$data = 'Text ini akan di encrypt menggunakan openssl! dan akan di decrypt dengan openssl juga';
+$secretKey = 'mykey';
 
-// Function to encrypt
-function encrypt($data, $secretKey, $iv)
+class MyCrypt {
+    public static function safe_encode ($text)
     {
-        return trim(strtr(base64_encode(openssl_encrypt($data, 'AES-256-CBC', $secretKey, 0, $iv)),'+/=', '-_,'));
+        return trim(strtr(base64_encode($text), '+/=', '-_,'));
     }
-$encrypted = encrypt($data, $secretKey, $iv); // Encrypted Result
-
-// Function to decrypt
-function decrypt($encrypted, $secretKey, $iv)
+    
+    public static function safe_decode($text)
     {
-        return trim(openssl_decrypt(base64_decode(strtr($encrypted,'-_,', '+/=')),'AES-256-CBC', $secretKey, 0, $iv));
+        return base64_decode(strtr($text, '-_,', '+/='));
     }
-$decrypted = decrypt($encrypted, $secretKey, $iv); // Decrypted Result
+    
+    public static function encrypt($data, $secretKey)
+    {
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
+        $cipher = openssl_encrypt($data, 'AES-256-CBC', $secretKey, 0, $iv);
+        
+        $safe_cipher = MyCrypt::safe_encode($cipher);
+        $safe_iv = MyCrypt::safe_encode($iv);
+        
+        return $safe_cipher . '::' . $safe_iv;
+    }
+    
+    public static function decrypt($encrypted, $secretKey)
+    {
+        $token = explode('::', $encrypted);
+        $cipher = MyCrypt::safe_decode($token[0]);
+        $iv = MyCrypt::safe_decode($token[1]);
+        
+        $plain = openssl_decrypt($cipher, 'AES-256-CBC', $secretKey, 0, $iv);
+        
+        return trim($plain);
+    }
+}
 
-var_dump ('Encrypted: ' . $encrypted); // Dump the value, or you can use echo
-var_dump ('Decrypted: ' . $decrypted); // Dump the value, or you can use echo
+$encrypted = MyCrypt::encrypt($data, $secretKey);
+$decrypted = MyCrypt::decrypt($encrypted, $secretKey);
+var_dump('Encrypted: ' . $encrypted);
+var_dump('Decrypted: ' . $decrypted);
 ?>
